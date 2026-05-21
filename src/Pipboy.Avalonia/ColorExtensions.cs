b67b2d06b2d62998fb48f28a8684c791c8ba0393
Easley <nevermore.wd@me.com>
2026-05-21 22:16:21 +0200

    add ColorExtensions

---
+++ src/Pipboy.Avalonia/ColorExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Media;
+using System;
+
+namespace Pipboy.Avalonia;
+
+public static class ColorExtensions
+{
+    public static Color WithAlpha(this Color color, byte alpha)
+    {
+        return Color.FromArgb(alpha, color.R, color.G, color.B);
+    }
+
+    public static Color WithOpacity(this Color color, double opacity)
+    {
+        byte alpha = (byte)(Math.Clamp(opacity, 0, 1) * 255);
+        return Color.FromArgb(alpha, color.R, color.G, color.B);
+    }
+}
